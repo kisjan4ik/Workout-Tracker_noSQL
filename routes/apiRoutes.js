bd81a0dd82a150cc754c568a59a7c90518b651b6
Turetska <oturetska@yahoo.com>
2020-08-03 16:06:13 -0400

    corrected put route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,15 @@
+const { Workout } = require("../models");
+
 // Create all the routes to listen to the requests from the front end.
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+
+
+
 
 // You will need at least Four (4) api routes:
 // GET ( "/api/workouts" ) - Get all workouts
 router.get("/api/workouts", (req, res) => {
+   
     Workout.find({})
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -13,10 +18,11 @@ router.get("/api/workouts", (req, res) => {
             res.json(err);
         });
 });
+
 // You will also need to figure out how to calculate the total duration from all exercises inside the exercises Array.
 // GET ( "/api/workouts/range" ) - Get the first 7 Workouts from the Workout table to be used in the "stats" page.
 router.get("/api/workouts/range", (req, res) => {
-    Workout.find().limit(7)
+    Workout.find({}).limit(7)
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
@@ -35,8 +41,9 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 // PUT ( "/api/workouts/:id" ) - Update a Workout
-router.put("api/workouts/:id", (req, res) => {
-    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+router.put("/api/workouts/:id", ({body,params}, res) => {
+    console.log("Workout ID :" + params.id, body);
+    Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } }, {new:true})
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
